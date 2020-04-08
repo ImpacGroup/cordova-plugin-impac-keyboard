e7675af246dfb9a1ca310dbbb0692d9cc4decae5
Felix Nievelstein <fn@impacgroup.de>
2020-04-08 10:06:57 +0200

    android set color and image to send button

--- src/android/IMPChatkeyboard.java
+++ src/android/IMPChatkeyboard.java
@@ -2,14 +2,20 @@ package de.impacgroup.cordovakeyboard;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.util.Base64;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -34,7 +40,7 @@ public class IMPChatkeyboard extends CordovaPlugin {
         switch (action) {
             case "showKeyboard":
                 if (chatInputView == null) {
-                    Application app=cordova.getActivity().getApplication();
+                    Application app = cordova.getActivity().getApplication();
                     String package_name = app.getPackageName();
                     Resources resources = app.getResources();
                     int ic = resources.getIdentifier("chat_input_view", "layout", package_name);
@@ -45,25 +51,47 @@ public class IMPChatkeyboard extends CordovaPlugin {
                         @Override
                         public void run() {
                             frameLayout.addView(chatInputView);
+                            callbackContext.success();
                         }
                     });
+
                 }
                 return true;
             case "onSendMessage":
                 return true;
             case "onInputChanged":
                 return true;
-            case "setColor":
-                String colorString = args.getString(0);
-                if (chatInputView != null && colorString != null) {
-                    Button button = chatInputView.findViewById(R.id.sendButton);
-                    button.setBackgroundColor(Color.parseColor(colorString));
+            case "setColor": {
+                final String colorString = args.getString(0);
+                final ImageButton button = getSendButton();
+                if (button != null && colorString != null) {
+                    this.cordova.getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            button.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(colorString)));
+                        }
+                    });
                     return true;
                 } else {
                     return false;
                 }
-            case "setImage":
-                return true;
+            }
+            case "setImage": {
+                String base64String = args.getString(0).replace("data:image/png;base64,", "");
+                final ImageButton button = getSendButton();
+                if (button != null) {
+                    byte[] decodedString = Base64.decode(base64String, Base64.NO_WRAP);
+                    final Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    this.cordova.getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            button.setImageBitmap(decodedByte);
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
             case "hideKeyboard":
                 if (chatInputView != null) {
                     frameLayout.removeView(chatInputView);
@@ -77,4 +105,13 @@ public class IMPChatkeyboard extends CordovaPlugin {
         return false;
     }
 
+    private @Nullable ImageButton getSendButton() {
+        if (chatInputView != null) {
+            Application app = cordova.getActivity().getApplication();
+            Resources resources = app.getResources();
+            int ic = resources.getIdentifier("sendButton", "id", app.getPackageName());
+            return chatInputView.findViewById(ic);
+        }
+        return null;
+    }
 }
